73bd6d5445d1d3c23799fafdbe985e1f2c22085b
Salha Benazir <105310587+wamosa@users.noreply.github.com>
2025-05-13 01:26:58 +0300

    Update clinical.sql

--- clinical.sql
+++ clinical.sql
@@ -1,25 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: May 12, 2025 at 02:55 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `clinical`
 --
+CREATE DATABASE clinical;
+
+USE clinical;
 
 -- --------------------------------------------------------
 
