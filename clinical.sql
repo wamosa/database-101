cb97bce7d1566945b9e9f6ed82a5243d5fc4a625
Salha Benazir <105310587+wamosa@users.noreply.github.com>
2025-05-13 01:25:37 +0300

    Add files via upload

---
+++ clinical.sql
@@ -0,0 +1,222 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 12, 2025 at 02:55 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clinical`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Appointments`
+--
+
+CREATE TABLE `Appointments` (
+  `AppointmentID` int(11) NOT NULL,
+  `PatientID` int(11) NOT NULL,
+  `DoctorID` int(11) NOT NULL,
+  `AppointmentDate` datetime NOT NULL,
+  `Reason` text DEFAULT NULL,
+  `Status` enum('Scheduled','Completed','Cancelled') DEFAULT 'Scheduled'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Departments`
+--
+
+CREATE TABLE `Departments` (
+  `DepartmentID` int(11) NOT NULL,
+  `DepartmentName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Doctors`
+--
+
+CREATE TABLE `Doctors` (
+  `DoctorID` int(11) NOT NULL,
+  `FullName` varchar(100) NOT NULL,
+  `Specialty` varchar(100) DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL,
+  `DepartmentID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Medications`
+--
+
+CREATE TABLE `Medications` (
+  `MedicationID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Patients`
+--
+
+CREATE TABLE `Patients` (
+  `PatientID` int(11) NOT NULL,
+  `FullName` varchar(100) NOT NULL,
+  `DOB` date DEFAULT NULL,
+  `Gender` enum('Male','Female','Other') DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Prescriptions`
+--
+
+CREATE TABLE `Prescriptions` (
+  `PrescriptionID` int(11) NOT NULL,
+  `AppointmentID` int(11) NOT NULL,
+  `MedicationID` int(11) NOT NULL,
+  `Dosage` varchar(50) DEFAULT NULL,
+  `Instructions` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Appointments`
+--
+ALTER TABLE `Appointments`
+  ADD PRIMARY KEY (`AppointmentID`),
+  ADD KEY `PatientID` (`PatientID`),
+  ADD KEY `DoctorID` (`DoctorID`);
+
+--
+-- Indexes for table `Departments`
+--
+ALTER TABLE `Departments`
+  ADD PRIMARY KEY (`DepartmentID`),
+  ADD UNIQUE KEY `DepartmentName` (`DepartmentName`);
+
+--
+-- Indexes for table `Doctors`
+--
+ALTER TABLE `Doctors`
+  ADD PRIMARY KEY (`DoctorID`),
+  ADD UNIQUE KEY `Phone` (`Phone`),
+  ADD KEY `DepartmentID` (`DepartmentID`);
+
+--
+-- Indexes for table `Medications`
+--
+ALTER TABLE `Medications`
+  ADD PRIMARY KEY (`MedicationID`),
+  ADD UNIQUE KEY `Name` (`Name`);
+
+--
+-- Indexes for table `Patients`
+--
+ALTER TABLE `Patients`
+  ADD PRIMARY KEY (`PatientID`),
+  ADD UNIQUE KEY `Phone` (`Phone`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `Prescriptions`
+--
+ALTER TABLE `Prescriptions`
+  ADD PRIMARY KEY (`PrescriptionID`),
+  ADD KEY `AppointmentID` (`AppointmentID`),
+  ADD KEY `MedicationID` (`MedicationID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Appointments`
+--
+ALTER TABLE `Appointments`
+  MODIFY `AppointmentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Departments`
+--
+ALTER TABLE `Departments`
+  MODIFY `DepartmentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Doctors`
+--
+ALTER TABLE `Doctors`
+  MODIFY `DoctorID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Medications`
+--
+ALTER TABLE `Medications`
+  MODIFY `MedicationID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Patients`
+--
+ALTER TABLE `Patients`
+  MODIFY `PatientID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Prescriptions`
+--
+ALTER TABLE `Prescriptions`
+  MODIFY `PrescriptionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Appointments`
+--
+ALTER TABLE `Appointments`
+  ADD CONSTRAINT `Appointments_ibfk_1` FOREIGN KEY (`PatientID`) REFERENCES `Patients` (`PatientID`),
+  ADD CONSTRAINT `Appointments_ibfk_2` FOREIGN KEY (`DoctorID`) REFERENCES `Doctors` (`DoctorID`);
+
+--
+-- Constraints for table `Doctors`
+--
+ALTER TABLE `Doctors`
+  ADD CONSTRAINT `Doctors_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `Departments` (`DepartmentID`);
+
+--
+-- Constraints for table `Prescriptions`
+--
+ALTER TABLE `Prescriptions`
+  ADD CONSTRAINT `Prescriptions_ibfk_1` FOREIGN KEY (`AppointmentID`) REFERENCES `Appointments` (`AppointmentID`),
+  ADD CONSTRAINT `Prescriptions_ibfk_2` FOREIGN KEY (`MedicationID`) REFERENCES `Medications` (`MedicationID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
